79d8a8e53fcf9bf727ecbded1b97832e1e154ecc
KlimkinArtem <55532526+KlimkinArtem@users.noreply.github.com>
2021-11-06 04:38:43 +0700

    Update Crazy_SantaCharacter.cpp

--- Source/Crazy_Santa/Crazy_SantaCharacter.cpp
+++ Source/Crazy_Santa/Crazy_SantaCharacter.cpp
@@ -1,140 +1,276 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+#include "Player/Santa.h"
 
-#include "Crazy_SantaCharacter.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
+#include "DrawDebugHelpers.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
-#include "Components/InputComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Math/TransformCalculus3D.h"
+#include "Sound/SoundCue.h"
 
-//////////////////////////////////////////////////////////////////////////
-// ACrazy_SantaCharacter
 
-ACrazy_SantaCharacter::ACrazy_SantaCharacter()
+ASanta::ASanta()
 {
-	// Set size for collision capsule
-	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-	// set our turn rates for input
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	
 	BaseTurnRate = 45.f;
 	BaseLookUpRate = 45.f;
-
-	// Don't rotate when the controller rotates. Let that just affect the camera.
+	
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
-
-	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
-
-	// Create a camera boom (pulls in towards the player if there is a collision)
+	
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
-	// Create a follow camera
+	CameraBoom->TargetArmLength = 300.0f;
+	CameraBoom->bUsePawnControlRotation = true;
+	
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false; 
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Input
-
-void ACrazy_SantaCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+void ASanta::BeginPlay()
 {
-	// Set up gameplay key bindings
+	Super::BeginPlay();
+
+	SpawnPick();
+}
+
+
+
+void ASanta::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void ASanta::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
 	check(PlayerInputComponent);
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &ACrazy_SantaCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ACrazy_SantaCharacter::MoveRight);
-
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASanta::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASanta::MoveRight);
+	
 	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &ACrazy_SantaCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("TurnRate", this, &ASanta::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &ACrazy_SantaCharacter::LookUpAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &ASanta::LookUpAtRate);
 
-	// handle touch devices
-	PlayerInputComponent->BindTouch(IE_Pressed, this, &ACrazy_SantaCharacter::TouchStarted);
-	PlayerInputComponent->BindTouch(IE_Released, this, &ACrazy_SantaCharacter::TouchStopped);
-
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ACrazy_SantaCharacter::OnResetVR);
+	PlayerInputComponent->BindAction("Debug", IE_Pressed, this, &ASanta::Debug);
+	
+	PlayerInputComponent->BindAction("Pick", IE_Pressed, this, &ASanta::SpawnPick);
+	PlayerInputComponent->BindAction("Bomb", IE_Pressed, this, &ASanta::SpawnBomb);
+	
+	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &ASanta::Attack);
 }
 
-
-void ACrazy_SantaCharacter::OnResetVR()
+void ASanta::TurnAtRate(float Rate)
 {
-	// If Crazy_Santa is added to a project via 'Add Feature' in the Unreal Editor the dependency on HeadMountedDisplay in Crazy_Santa.Build.cs is not automatically propagated
-	// and a linker error will result.
-	// You will need to either:
-	//		Add "HeadMountedDisplay" to [YourProject].Build.cs PublicDependencyModuleNames in order to build successfully (appropriate if supporting VR).
-	// or:
-	//		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
-
-void ACrazy_SantaCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
-{
-	Jump();
-}
-
-void ACrazy_SantaCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
-{
-		StopJumping();
-}
-
-void ACrazy_SantaCharacter::TurnAtRate(float Rate)
-{
-	// calculate delta for this frame from the rate information
 	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
 }
 
-void ACrazy_SantaCharacter::LookUpAtRate(float Rate)
+void ASanta::LookUpAtRate(float Rate)
 {
-	// calculate delta for this frame from the rate information
 	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
-void ACrazy_SantaCharacter::MoveForward(float Value)
+void ASanta::MoveForward(float Value)
 {
 	if ((Controller != nullptr) && (Value != 0.0f))
 	{
-		// find out which way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		// get forward vector
+		
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 		AddMovementInput(Direction, Value);
 	}
 }
 
-void ACrazy_SantaCharacter::MoveRight(float Value)
+void ASanta::MoveRight(float Value)
 {
 	if ( (Controller != nullptr) && (Value != 0.0f) )
 	{
-		// find out which way is right
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
+		
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-		// add movement in that direction
 		AddMovementInput(Direction, Value);
 	}
 }
+
+
+
+void ASanta::Debug()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Debug")));
+}
+
+
+void ASanta::SpawnPick()
+{
+	if(!GEngine) return;
+	
+	if(WeaponType == PICK || !bResetAnimMontage)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("Pick Is valid")));
+		return;
+	}else if(WeaponType == BOMB)
+	{
+		Bomb->Destroy();
+	}
+
+	
+	WeaponType = PICK;
+	
+	FString SocketName = "PickSocket";
+	FVector Location = GetMesh()->GetSocketLocation(*SocketName);
+	FRotator Rotation = GetMesh()->GetSocketRotation(*SocketName);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Socket loc = %s"), *Location.ToString()));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Socket rot = %s"), *Rotation.ToString()));
+	
+	
+	FActorSpawnParameters SpawnInfo;
+	FAttachmentTransformRules AttachRules = FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
+	
+	Pick = GetWorld()->SpawnActor<AActor>(Weapons[0],Location, Rotation, SpawnInfo);
+	
+	Pick->AttachToComponent(GetMesh(), AttachRules, *SocketName);
+}
+
+void ASanta::SpawnBomb()
+{
+	if(Bombs <= 0 || !bResetAnimMontage || WeaponType == BOMB)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("Bomb Is valid")));
+		return;
+	}
+	
+	Pick->Destroy();
+	
+
+    	
+	
+
+	WeaponType = BOMB;
+	
+	FString SocketName = "BombSocket";
+	FVector Location = GetMesh()->GetSocketLocation(*SocketName);
+	FRotator Rotation = GetMesh()->GetSocketRotation(*SocketName);
+
+	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Socket loc = %s"), *Location.ToString()));
+	
+
+	FActorSpawnParameters SpawnInfo;
+	FAttachmentTransformRules AttachRules = FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
+	
+	Bomb = GetWorld()->SpawnActor<AActor>(Weapons[1],Location, Rotation, SpawnInfo);
+	
+	Bomb->AttachToComponent(GetMesh(), AttachRules, *SocketName);
+}
+
+void ASanta::Attack()
+{
+	switch (WeaponType)
+	{
+		case PICK:
+			//CallFunctionByNameWithArguments(TEXT("PickAttackAnim"), ar, NULL, true);
+			if(bResetAnimMontage)
+			{
+				PlayAnimMontage(AnimMontage);
+				bResetAnimMontage = false;
+			}
+			
+			break;
+		case BOMB:
+			BombAttack();
+			break;
+		default:
+			break;
+	}
+}
+
+void ASanta::PickAttack(float Radius, int32 Segments, bool bDrawDebugSphere)
+{
+	bPickCue = false;
+
+	FString SocketName = "PickAttackSocket";
+	FVector Location = GetMesh()->GetSocketLocation(*SocketName);
+	FRotator Rotation = GetMesh()->GetSocketRotation(*SocketName);
+
+	FActorSpawnParameters SpawnInfo;
+	FAttachmentTransformRules AttachRules = FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
+	
+	auto Force = GetWorld()->SpawnActor<AActor>(RadialForce[0],Location, Rotation, SpawnInfo);
+	Force->Destroy();
+
+	FVector Start = GetMesh()->GetSocketLocation(TEXT("PickAttackSocket"));
+
+	if(bDrawDebugSphere) DrawDebugSphere(GetWorld(), Start, Radius, Segments, FColor(181,0,0), true, 2, 0, 2);
+	
+	
+	TArray<FHitResult> OutHits;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+	
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(Radius);
+	
+	bool bIsHit = GetWorld()->SweepMultiByChannel(OutHits, Start, Start, FQuat::Identity, ECC_Pawn, Sphere, Params);
+	
+	if (bIsHit)
+	{
+		
+		for (auto& Hit : OutHits)
+		{
+			if (GEngine) 
+			{
+				if(Hit.Actor->ActorHasTag("Pick"))
+				{
+					if(bPickCue) return;
+					GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("PICKkkk")));
+					bPickCue = true;
+					UGameplayStatics::SpawnSound2D(this, PickCue[0], 1);
+					return;
+				}
+			}						
+		}
+	}
+	
+	UGameplayStatics::SpawnSound2D(this, PickCue[1], 1);
+}
+
+void ASanta::BombAttack()
+{
+	if(!bResetAnimMontage) return;
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("BombAttack")));
+
+	FString SocketName = "BombSocket";
+	FVector Location = GetMesh()->GetSocketLocation(*SocketName);
+	FRotator Rotation = GetMesh()->GetSocketRotation(*SocketName);
+	
+	FActorSpawnParameters SpawnInfo;
+	
+	GetWorld()->SpawnActor<AActor>(PH_Bomb, Location, Rotation, SpawnInfo);
+
+	Bombs -= 1;
+
+	if(Bombs <= 0) SpawnPick();
+
+}
+
